90e02f442fc8e045cede2671443867ed1ce4a648
AnthGrow <122587083+AnthGrow@users.noreply.github.com>
2023-04-01 10:31:45 -0500

    started adding inquire prompts - have ERR_REQUIRE_ESM to resolve

---
+++ index.js
@@ -0,0 +1,31 @@
+const inquirer = require("inquirer");
+
+//prompt for text
+
+inquirer
+  .prompt([
+    {
+      name: "text",
+      message: "Please, enter up to three characters",
+    },
+
+    {
+      name: "textColor",
+      message: "Please select a text color",
+    },
+
+    {
+      type: "list",
+      name: "shape",
+      message: "Select a shape",
+      choices: ["circle", "triangle", "square"],
+    },
+
+    {
+      name: "bgcolor",
+      message: "select a background color",
+    },
+  ])
+  .then((answers) => {
+    console.info("text", answers.text);
+  });
